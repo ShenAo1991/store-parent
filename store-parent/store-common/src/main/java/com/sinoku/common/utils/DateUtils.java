3d1c91db72bc0dc235cd6c31bd5048e6b895d19f
sino_ku <shen.ao@immomo.com>
2019-07-23 12:15:18 +0800

    ytest

---
+++ store-parent/store-common/src/main/java/com/sinoku/common/utils/DateUtils.java
@@ -0,0 +1,41 @@
+package com.sinoku.common.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @ClassName DateUtils
+ * @Description 日期工具类
+ * @Author <a mailto:"shen.ao@immomo.com">shen.ao</a>
+ * @Date 2019/7/23 下午12:09
+ **/
+public class DateUtils {
+
+
+  /**
+   * format date 2 str
+   * @param d
+   * @param ptn
+   * @return
+   */
+  public static String formatDate(Date d, String ptn) {
+    SimpleDateFormat sf = new SimpleDateFormat(ptn);
+    return sf.format(d);
+  }
+
+  /**
+   *
+   * @param str
+   * @param ptn
+   * @return
+   * @throws ParseException
+   */
+  public static Date parse(String str, String ptn) throws ParseException {
+    SimpleDateFormat sf = new SimpleDateFormat(ptn);
+    return sf.parse(str);
+  }
+
+
+
+}
